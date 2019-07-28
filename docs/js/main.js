b9deba2efdbd6dd73cbd4a882b10e0f9fda880af
Jean-Baptiste Bernus <jbernus@gmail.com>
2019-07-28 22:30:50 +0200

    Publish to Github Pages

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,24 +1,21 @@
+const mapBoxUrl = 'https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token=' + config.token;
+const mapInitCoordinates = [47.02167640440166, 8.653083890676498];
 
+const startIcon = L.divIcon({className: 'start_icon'});
+const finishIcon = L.divIcon({className: 'finish_icon'});
 
-let colsList = '';
-let  selectedCol;
-let isSelectedCol = false;
-let hoveredCol;
-let colsL = [];
-let list = document.getElementById('list');
-var mapBoxUrl = 'https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token=' + config.token;
+const colNormalColor = 'blue';
+const colHoverColor = 'red';
+const selectedListItemClass = 'selected';
+const startMarkerTitle = 'Start';
+const finishMarkerTitle = 'Finish';
 
-var mapInitCoordinates = [47.02167640440166, 8.653083890676498];
-
-var streets = L.tileLayer(mapBoxUrl, {id: 'streets-v11'});
-var outdoors = L.tileLayer(mapBoxUrl, {id: 'outdoors-v9'});
-var satellite = L.tileLayer(mapBoxUrl, {id: 'satellite-streets-v11'});
+const streets = L.tileLayer(mapBoxUrl, {id: 'streets-v11'});
+const outdoors = L.tileLayer(mapBoxUrl, {id: 'outdoors-v9'});
+const satellite = L.tileLayer(mapBoxUrl, {id: 'satellite-streets-v11'});
 // https://docs.mapbox.com/api/maps/#styles
 
-var startIcon = L.divIcon({className: 'start_icon'});
-var finishIcon = L.divIcon({className: 'finish_icon'});
-
-var baseMaps = {
+const baseMaps = {
     "Outdoors": outdoors,
     "Streets": streets,
     "Satellite": satellite
@@ -26,7 +23,14 @@ var baseMaps = {
 
 const credits = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
 
-let list_items = document.getElementsByClassName('list_item');
+const list_items = document.getElementsByClassName('list_item');
+
+let colsList = '';
+let  selectedCol;
+let isSelectedCol = false;
+let hoveredCol;
+let colsL = [];
+let list = document.getElementById('list');
 
 function generateMiddleLatLng() {
     data.cols = data.cols.map((col) => {
@@ -42,8 +46,7 @@ function generateMiddleLatLng() {
 
 function setColOpacity(col, opacityLevel) {
     col.setStyle({
-        opacity: opacityLevel,
-        weight: 4
+        opacity: opacityLevel
     });
     col.startMarker.setOpacity(opacityLevel);
     col.finishMarker.setOpacity(opacityLevel);
@@ -56,12 +59,11 @@ let map = L.map('map', {
     layers: [streets]
     }).setView(mapInitCoordinates, 9);
 
+L.control.layers(baseMaps).addTo(map);
+map.zoomControl.setPosition('bottomright');
+
 var creditsOnMap = L.control.attribution().addTo(map);
 creditsOnMap.addAttribution(credits);
-
-L.control.layers(baseMaps).addTo(map);
-
-map.zoomControl.setPosition('bottomright');
 
 for (let col of data.cols) {
 
@@ -80,20 +82,20 @@ for (let col of data.cols) {
     let colPolyline = L.polyline(
         coordinates,
         {
-            color: 'blue',
+            color: colNormalColor,
             weight: 4,
             opacity: .7,
             lineJoin: 'round'
         }
-    ).addTo(map).bindPopup(col.name, {autoPan: false});
+    ).addTo(map).bindPopup(col.name, {autoPan: false}).on('click', function() { console.log('col', col.name);});
 
     let startMarker = L.marker([col.start_latlng[0], col.start_latlng[1]],{
         icon: startIcon,
-        title: 'start'
+        title: startMarkerTitle
     }).addTo(map);
     let finishMarker = L.marker([col.end_latlng[0], col.end_latlng[1]],{
         icon: finishIcon,
-        title: 'Finish'
+        title: finishMarkerTitle
     }).addTo(map);
 
     colPolyline.name = col.name;
@@ -105,23 +107,26 @@ for (let col of data.cols) {
 
 list.innerHTML = colsList;
 
-function mouseenterCol() {
-    let colName = this.getAttribute('data-name');
+function removeSelectedState() {
+    Array.from(list_items).forEach(function(list_item) {
+        list_item.parentElement.classList.remove(selectedListItemClass);
+    });
+}
+
+function passHover(col, color) {
+    let colName = col.getAttribute('data-name');
     hoveredCol = colsL.find((colL) => colL.name === colName);
-    if(hoveredCol === selectedCol) {
-        return;
-    }
     hoveredCol.setStyle({
-        weight: 6
+        color: color
     })
 }
 
+function mouseenterCol() {
+    passHover(this, colHoverColor);
+}
+
 function mouseleaveCol() {
-    let colName = this.getAttribute('data-name');
-    hoveredCol = colsL.find((colL) => colL.name === colName);
-    hoveredCol.setStyle({
-        weight: 4
-    })
+    passHover(this, colNormalColor);
 }
 
 function zoomTo(e, l) {
@@ -130,11 +135,9 @@ function zoomTo(e, l) {
     let colName = this.getAttribute('data-name');
     selectedCol = colsL.find((colL) => colL.name === colName);
 
-    Array.from(list_items).forEach(function(list_item) {
-        list_item.parentElement.classList.remove('selected');
-    });
+    removeSelectedState();
 
-    this.parentElement.classList.add('selected');
+    this.parentElement.classList.add(selectedListItemClass);
 
     isSelectedCol = false;
 
@@ -157,9 +160,7 @@ map.on('moveend', function(e){
         isSelectedCol = false;
         selectedCol = undefined;
 
-        Array.from(list_items).forEach(function(list_item) {
-            list_item.parentElement.classList.remove('selected');
-        });
+        removeSelectedState();
     }
 
     if(typeof selectedCol !== 'undefined') {
